75c8541e0c8dc4799bc87ef9be733f6a0d18879d
Caio Andres <caioandresgit@outlook.com>
2025-04-15 20:31:53 -0300

    2nd

--- src/app/components/list-books/list-books.component.ts
+++ src/app/components/list-books/list-books.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book, LiteraryGenre } from '../book/book';
+import { books } from '../../mock-books';
 
 @Component({
   selector: 'app-list-books',
   imports: [],
   templateUrl: './list-books.component.html',
-  styleUrl: './list-books.component.css'
+  styleUrl: './list-books.component.css',
 })
-export class ListBooksComponent {
+export class ListBooksComponent implements OnInit {
+  genre: LiteraryGenre[] = [];
+  bookPerGenre: Map<string, Book[]> = new Map();
 
+  ngOnInit(): void {
+    this.bookPerGenre = new Map();
+
+    books.forEach((book) => {
+      const genreId = book.genre.id;
+      if (!this.bookPerGenre.has(genreId)) {
+        this.bookPerGenre.set(genreId, []);
+      }
+
+      this.bookPerGenre.get(genreId)?.push(book);
+    });
+
+    this.genre = [
+      {
+          id: 'romance',
+          value: "Romance",
+          books: this.bookPerGenre.get("romance") ?? []
+      },
+      {
+          id: 'misterio',
+          value: 'Mistério',
+          books: this.bookPerGenre.get("misterio") ?? []
+      },
+      {
+          id: 'fantasia',
+          value: 'Fantasia',
+          books: this.bookPerGenre.get("fantasia") ?? []
+      },
+      {
+          id: 'ficcao-cientifica',
+          value: 'Ficção Científica',
+          books: this.bookPerGenre.get("ficcao-cientifica") ?? []
+      },
+      {
+          id: 'tecnicos',
+          value: 'Técnicos',
+          books: this.bookPerGenre.get("tecnicos") ?? []
+      },
+  ]
+
+  console.log(this.genre)
+  }
 }
--- src/app/mock-books.ts
+++ src/app/mock-books.ts
@@ -12,4 +12,37 @@ export const books: Book[] = [
       favorite: false,
       image: 'https://m.media-amazon.com/images/I/61I24wOsn8L._AC_UF1000,1000_QL80_.jpg'
     },
+    {
+      title: 'Jorge',
+      author: 'Jorginho',
+      genre: {
+        id: 'romance',
+        value: 'Romance',
+        books: []
+      },
+      favorite: false,
+      image: 'https://m.media-amazon.com/images/I/61I24wOsn8L._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      title: 'As',
+      author: 'Vf',
+      genre: {
+        id: 'romance',
+        value: 'Romance',
+        books: []
+      },
+      favorite: false,
+      image: 'https://m.media-amazon.com/images/I/61I24wOsn8L._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      title: 'aaaaa',
+      author: 'sarve',
+      genre: {
+        id: 'romance',
+        value: 'Romance',
+        books: []
+      },
+      favorite: false,
+      image: 'https://m.media-amazon.com/images/I/61I24wOsn8L._AC_UF1000,1000_QL80_.jpg'
+    },
 ]
